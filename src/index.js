8e47c21a067e95e3d4afd1deec96c2911c0723f7
neelg07 <ineel7762@gmail.com>
2023-02-17 13:15:58 -0500

    Create template.js file to render main template on page load; Export main function into index.js for use;

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './stylesheets/style.css';
+import renderTemplate from './modules/template';
 
 console.log('initialize webpack');
+
+document.body.appendChild(renderTemplate());
---
+++ src/modules/template.js
@@ -0,0 +1,72 @@
+function renderTemplate() {
+    const content = document.createElement('div');
+    content.setAttribute('id', 'content');
+
+    const navDiv = document.createElement('div');
+    navDiv.classList.add('nav');
+    navDiv.appendChild(createNav());
+
+    const background = document.createElement('img');
+    background.src = '../src/images/sushi-restaurant-bg.jpg';
+    background.alt = 'Japanese Restaurant Background';
+    background.setAttribute('id', 'sushi-bg');
+
+    content.appendChild(navDiv);
+    content.appendChild(background);
+
+    return content;
+}
+
+function createNav() {
+    const nav = document.createElement('nav');
+    nav.appendChild(createHeader());
+    nav.appendChild(createTabs());
+    
+    return nav;
+}
+
+function createHeader() {
+    // create div to sandwich header
+    const header = document.createElement('div');
+    header.classList.add('header');
+
+    // create sushi icon img
+    const sushiIcon = document.createElement('img');
+    sushiIcon.src = '../src/images/sushi-icon.png';
+    sushiIcon.alt = 'Sushi Roll';
+    sushiIcon.setAttribute('id', 'sushi-icon');
+
+    // create h1 name of restaurant
+    const restName = document.createElement('h1');
+    restName.append('Sonic Sushi');
+
+    // Append elements to header div and return
+    header.appendChild(sushiIcon);
+    header.appendChild(restName);
+
+    return header;
+}
+
+function createTabs() {
+    const list = document.createElement('ul');
+
+    let tabs = ['Home', 'Menu', 'Contact'];
+    for (let tab of tabs) {
+        list.appendChild(createLink(tab));
+    }
+
+    return list;
+}
+
+function createLink(name) {
+    const listItem = document.createElement('li');
+    // Add href attr. (change later)
+    const link = document.createElement('a');
+    link.href = '#';
+    link.append(name);
+
+    listItem.appendChild(link);
+    return listItem;
+}
+
+export default renderTemplate;
