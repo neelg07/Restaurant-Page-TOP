a4c18747e5f4f34dd9900891601616e28813ab76
neelg07 <ineel7762@gmail.com>
2023-02-18 12:26:40 -0500

    Add source map to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     mode: 'development',
+    devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Sushi Sonic'
