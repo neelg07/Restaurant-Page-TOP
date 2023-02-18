057750c7994311afe8de9b43a23693e9090cc79a
neelg07 <ineel7762@gmail.com>
2023-02-18 12:22:49 -0500

    Add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
     mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Sushi Sonic'
+        }),
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
