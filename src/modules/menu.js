f14bb81a630eb3ef90566f1861c64262555708e8
neelg07 <ineel7762@gmail.com>
2023-02-28 10:36:56 -0500

    Add menu object with all sub objects included

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -5,30 +5,32 @@ class MenuItem {
         this.description = description;
         this.price = price;
     }
-}
+};
 
 const appetizers = {
     rice: new MenuItem('Rice', 3.50, 'Steaming hot bowl of white Jasmine rice'),
     shumai: new MenuItem('Shumai', 3.99, 'Steamed shrimp dumplings'),
     edamame: new MenuItem('Edamame', 2.99, 'Steamed soybean with sea salt'),
     salad: new MenuItem('House Salad', 3.99, 'Romaine lettuce, carrot, tomato, cucumber with ginger dressing'), 
-}
+};
 
 const sushi = {
     vegas: new MenuItem('Las Vegas', 9.99, 'Crab meat, cucumber, avocado, shredded crab stick, spicy mayo, sweet crunch'),
     dynamite: new MenuItem('Dynamite (Baked)', 10.99, 'Crab meat, cucumber, avocado, white fish, crab, shrimp with spicy mayo, sweet'),
     veggie: new MenuItem('Veggie Roll', 8.99, 'Cucumber, lettuce, tomato, slice avocado, spicy mayo, white crunch'),
     imperial: new MenuItem('Imperial Roll', 13.99, 'Spicy tuna, shrimp tempura, cream cheese, avocado, salmon, crab meat with spicy mayo, sweet'),
-}
+};
 
 const beverages = {
     tea: new MenuItem('Hot Tea', 2.50, 'Green, Black, Jasmine, Chamomile'),
     soda: new MenuItem('Fountain Drinks', 2.99, 'Pepsi, Mt. Dew, Lemonade, Root Beer, Iced Tea, Dr. Pepper'),
     beer: new MenuItem('Beer', 3.50, 'Sapporo, Kirin, Blue Moon, Corona'),
     sake: new MenuItem('Sake', 8.00, 'Shiro sasa, nigori, peach sparkling, pineapple sparkling, yuzu sparkling'),
-}
+};
 
 const desert = {
     iceCream: new MenuItem('Ice Cream', 5.99, 'Vanilla, Chocolate, Strawberry, Mint, Cookies n\' Creme'),
     mochi: new MenuItem('Mochi Ice Cream', 6.99, 'Vanilla, Chocolate, Strawberry, Green Tea'),
-}
+};
+
+const menu = {appetizers, sushi, beverages, desert};
