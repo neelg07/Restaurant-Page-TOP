ae2814b231a60414b8f9e016394ab9a25e8b927e
neelg07 <ineel7762@gmail.com>
2023-02-28 22:19:53 -0500

    Create js file to create menu section

---
+++ src/modules/menu-render.js
@@ -0,0 +1,157 @@
+import menu from './menu';
+
+function createMenuSection() {
+    const menuDiv = document.createElement('div');
+    menuDiv.classList.add('menu');
+
+    // Append all menu sections and headers //
+    menuDiv.appendChild(createAppsHeader());
+    menuDiv.appendChild(createAppsDiv());
+
+    menuDiv.appendChild(createSushiHeader());
+    menuDiv.appendChild(createSushiDiv());
+
+    menuDiv.appendChild(createBeveragesHeader());
+    menuDiv.appendChild(createBeveragesDiv());
+
+    menuDiv.appendChild(createDessertHeader());
+    menuDiv.appendChild(createDessertDiv());
+
+    return menuDiv;
+}
+
+// Appetizers //
+
+function createAppsHeader() {
+    const appsHeader = document.createElement('h1');
+    appsHeader.append('Appetizers');
+
+    return appsHeader;
+}
+
+function createAppsDiv() {
+    const appsDiv = document.createElement('div');
+    appsDiv.classList.add('appetizers');
+
+    // Iterate thru all items in appetizer section and append to div //
+    for (let item in menu.appetizers) {
+        appsDiv.appendChild(createFoodDiv(item));
+    }
+    return appsDiv;
+}
+
+// Sushi //
+
+function createSushiHeader() {
+    const sushiHeader = document.createElement('h1');
+    sushiHeader.append('Sushi Rolls');
+
+    return sushiHeader;
+}
+
+function createSushiDiv() {
+    const sushiDiv = document.createElement('div');
+    sushiDiv.classList.add('sushi');
+
+    for (let item in menu.sushi) {
+        sushiDiv.appendChild(createFoodDiv(item));
+    }
+    return sushiDiv;
+}
+
+// Beverages //
+
+function createBeveragesHeader() {
+    const bevHeader = document.createElement('h1');
+    bevHeader.append('Beverages');
+
+    return bevHeader;
+}
+
+function createBeveragesDiv() {
+    const bevDiv = document.createElement('div');
+    bevDiv.classList.add('beverages');
+
+    for (let item in menu.beverages) {
+        bevDiv.appendChild(createFoodDiv(item));
+    }
+    return bevDiv;
+}
+
+// Dessert //
+
+function createDessertHeader() {
+    const dessertHeader = document.createElement('h1');
+    dessertHeader.append('Dessert');
+
+    return dessertHeader;
+}
+
+function createDessertDiv() {
+    const dessertDiv = document.createElement('div');
+    dessertDiv.classList.add('dessert');
+
+    for (let item in menu.dessert) {
+        dessertDiv.appendChild(createFoodDiv(item));
+    }
+    return dessertDiv;
+}
+
+// FoodDiv func and all underlying func used for apps, sushi, beverages, and desert sections //
+
+function createFoodDiv(food) {
+    const foodDiv = document.createElement('div');
+    foodDiv.classList.add(`${food}`);
+
+    foodDiv.appendChild(createFoodInfo(food));
+    foodDiv.appendChild(createFoodDescription(food));
+
+    return foodDiv;
+}
+
+function createFoodInfo(food) {
+    const infoDiv = document.createElement('div');
+    infoDiv.classList.add('info');
+
+    infoDiv.appendChild(createFoodImg(food));
+    infoDiv.appendChild(createNamePriceDiv(food));
+
+    return infoDiv;
+}
+
+function createFoodImg(food) {
+    const foodImg = document.createElement('img');
+    foodImg.src = `../src/images/menu-images/${food}.jpg`;
+    foodImg.alt = `${food}`;
+    foodImg.setAttribute('id', `${food}`);
+
+    return foodImg;
+}
+
+function createNamePriceDiv(food) {
+    const namePriceDiv = document.createElement('div');
+    namePriceDiv.classList.add('name-price');
+
+    const foodName = document.createElement('h2');
+    foodName.append(food.name);
+    namePriceDiv.appendChild(foodName);
+
+    const foodPrice = document.createElement('h2');
+    foodPrice.append(`$ ${food.price}`);
+    namePriceDiv.appendChild(foodPrice);
+
+    return namePriceDiv;
+}
+
+function createFoodDescription(food) {
+    const descriptionDiv = document.createElement('div');
+    descriptionDiv.classList.add('description');
+
+    const descriptionPara = document.createElement('p');
+    descriptionPara.append(food.description);
+    descriptionDiv.appendChild(descriptionPara);
+    
+    return descriptionDiv;
+}
+
+export default createMenuSection;
