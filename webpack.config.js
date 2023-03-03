38756969c32ab6a1e1ecd098b29262bdf64a7ed6
neelg07 <ineel7762@gmail.com>
2023-03-02 21:31:23 -0500

    Change output to /docs

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        static: './dist',
+        static: './docs',
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -18,7 +18,7 @@ module.exports = {
     ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         clean: true,
     },
     optimization: {
